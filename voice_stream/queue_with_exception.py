295327145ba862a01f9b5e2ccd27df8a84da3bb4
Dave DeCaprio <daved@alum.mit.edu>
2024-01-12 16:33:32 -0600

    readthedocs on python 3.10

--- voice_stream/queue_with_exception.py
+++ voice_stream/queue_with_exception.py
@@ -10,8 +10,48 @@ logger = logging.getLogger(__name__)
 
 
 class QueueWithException(asyncio.Queue):
-    """A queue that propagates exceptions.
-    If a None is enqueued and the exception is set, then the exception is thrown on put.
+    """
+    A specialized asyncio.Queue that propagates exceptions to the consumers.
+
+    This queue enhances the standard asyncio.Queue by handling exceptions. When an exception
+    is set in the queue via `set_exception()`, it is propagated to the consumers either when they call `get()` or
+    `get_nowait()`.
+
+    Methods
+    -------
+    enqueue_iterator(self, async_iter: AsyncIterator):
+        Enqueues items from an asynchronous iterator to the queue.
+        Parameters:
+            async_iter : AsyncIterator
+                An asynchronous iterator whose items will be enqueued.
+
+    get(self):
+        Asynchronously gets an item from the queue, propagating any exception if present.
+
+    get_nowait(self):
+        Gets an item from the queue without blocking, propagating any exception if present.
+
+    set_exception(self, exception: Exception):
+        Sets an exception to be propagated to consumers.
+        Parameters:
+            exception : Exception
+                The exception to be propagated.
+
+    Raises
+    ------
+    ValueError
+        If the provided async_iter is not an AsyncIterator.
+    Exception
+        The stored exception, if any, when `get` or `get_nowait` is called.
+
+    Examples
+    --------
+    >>> queue = queue_source()
+    >>> queue.put(1)
+    >>> queue.put(2)
+    >>> queue.set_exception(RuntimeError("Test Error"))
+    >>> await array_sink(queue)
+    Caught exception: Test Error
     """
 
     def __init__(self):
